b33462e6aef46a52c9fc7946d39fc546ea82a4f9
olifj1 <olifj1@gmail.com>
2026-08-18 21:22:40 +0100

    Add files via upload

--- home.js
+++ home.js
@@ -11,7 +11,7 @@ const iconSvg = {
   "game-05": `<svg viewBox="0 0 48 48"><rect x="10" y="8" width="28" height="32" rx="5"/><path d="M15 15h18M16 23h4M28 23h4M16 30h4M28 30h4"/></svg>`,
   "game-06": `<svg viewBox="0 0 48 48"><path d="M24 7c5 5 8 11 8 17l-8 8-8-8c0-6 3-12 8-17zM18 28l-5 8 8-2M30 28l5 8-8-2M24 17v8"/></svg>`,
   "game-07": `<svg viewBox="0 0 48 48"><path d="M8 30h27l4 6H13zM14 30V19h13v11M27 23h7l4 7M18 19v-6h10M15 36a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM34 36a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"/></svg>`,
-  "game-08": `<svg viewBox="0 0 48 48"><path d="M8 24h15M29 24h11M23 14l8 10-8 10zM10 18v12"/></svg>,
+  "game-08": `<svg viewBox="0 0 48 48"><path d="M8 24h15M29 24h11M23 14l8 10-8 10zM10 18v12"/></svg>`,
   "game-09": `<svg viewBox="0 0 48 48"><path d="M7 25h10l8-10 4 1-4 9h10l6-5 2 2-5 7H25l4 10-4 1-8-11H7z"/><path d="M12 22v6"/></svg>`
 };
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gamehub-v1.0.47";
+const CACHE = "gamehub-v1.0.48";
 const APP_SHELL = [
   "./",
   "./index.html",
